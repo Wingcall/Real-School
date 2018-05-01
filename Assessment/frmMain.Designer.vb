f17774ff775553298ff2225698bf8769d48b614e
Wingcall <wingcall@emmaus.qld.edu.au>
2018-05-02 09:15:25 +1000

    Commit 2

--- Assessment/frmMain.Designer.vb
+++ Assessment/frmMain.Designer.vb
@@ -50,9 +50,9 @@ Partial Class frmMain
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(130, 162)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.Size = New System.Drawing.Size(90, 13)
         Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Push "
+        Me.Label1.Text = "Push MThe whay"
         '
         'frmMain
         '
