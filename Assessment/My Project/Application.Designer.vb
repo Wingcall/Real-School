8989ef3f9c64f75f1b1d290e29531fe1e2af64d0
snortings <bjdingley@hotmail.com>
2018-05-09 08:40:19 +1000

    Test

--- Assessment/My Project/Application.Designer.vb
+++ Assessment/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Assessment.frmGame
+            Me.MainForm = Global.Assessment.frmMain
         End Sub
     End Class
 End Namespace
