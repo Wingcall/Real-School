ed88a947d25b668d6ff0ddae236295e53697bf7c
Wingcall <wingcall@emmaus.qld.edu.au>
2018-05-10 12:44:53 +1000

    Cals Debug Stuff

---
+++ Assessment/Forms/frmDebug.vb
@@ -0,0 +1,8 @@
+﻿Public Class frmDebug
+
+    Private Sub btnPlayer1_Click(sender As Object, e As EventArgs) Handles btnPlayer1.Click, btnPlayer2.Click
+        'loadUserDB()
+
+        lblName.Text = sender.Tag
+    End Sub
+End Class
