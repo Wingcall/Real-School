42f71ac4e2ea8fc3e97dcc8720f0dadafb332ea5
Wingcall <wingcall@emmaus.qld.edu.au>
2018-05-12 18:01:06 +1000

    Got the bones of the game form running

--- Assessment/Forms/frmGame.vb
+++ Assessment/Forms/frmGame.vb
@@ -1,21 +1,48 @@
 ﻿Public Class frmGame
-    Dim localScore
+    Dim maxGuesses As Integer = 3
+    Dim letterGuesses As String = ""
+    Dim badGuesses As Integer
 
     Private Sub frmGame_Load(sender As Object, e As EventArgs) Handles MyBase.Load, btnLoadButton.Click
         loadDB()
         AddHandler Me.FormClosing, AddressOf formEvents.FormClosing
 
-        plrWord = wordLists(catID).randomWord()
+        letterGuesses = ""
+        badGuesses = maxGuesses
+
+        plrWord = New word("abFH", "cd") 'wordLists(catID).randomWord()
         lblWord.Text = plrWord.wordVal
         lblHint.Text = plrWord.hint
         lblCat.Text = wordLists(catID).name
         lblName.Text = userInfo(playerID).name
         lblScore.Text = userInfo(playerID).score
-        lblHyphen.Text = plrWord.hyphenWord("BIl")
+        lblHyphen.Text = plrWord.hyphenWord()
+
+        pnlLetters.Enabled = True
+        For Each Letter As Label In pnlLetters.Controls
+            If sender.name = "frmGame" Then
+                AddHandler Letter.Click, AddressOf Me.lblLetterS_Click
+            End If
+            Letter.Enabled = True
+        Next
 
     End Sub
 
-    Private Sub lblLetterS_Click(sender As Object, e As EventArgs) Handles lblLetterA.Click, lblLetterB.Click, lblLetterC.Click, lblLetterD.Click
-
+    Private Sub lblLetterS_Click(sender As Label, e As EventArgs)
+        sender.Enabled = False
+        If plrWord.wordVal.Contains(sender.Text) Then
+            letterGuesses += sender.Text
+            lblHyphen.Text = plrWord.hyphenWord(letterGuesses)
+            If Not lblHyphen.Text.Contains("-") Then
+                pnlLetters.Enabled = False
+                MsgBox("Game Win!")
+            End If
+        Else
+            badGuesses -= 1
+            If badGuesses <= 0 Then
+                pnlLetters.Enabled = False
+                MsgBox("Game Lost")
+            End If
+        End If
     End Sub
 End Class
