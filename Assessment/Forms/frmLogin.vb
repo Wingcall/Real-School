038c9c7967b3d8f015990efa6440cc11be23b634
snortings <bjdingley@hotmail.com>
2018-05-14 14:15:36 +1000

    message box

--- Assessment/Forms/frmLogin.vb
+++ Assessment/Forms/frmLogin.vb
@@ -18,16 +18,19 @@
     End Sub
 
     Private Sub btnDelPlayer_Click(sender As Object, e As EventArgs) Handles btnDelPlayer.Click
-        userInfo(playerID).del()        'Deletes the player info selected by selected.
-        rdoPlayer1.Text = userInfo(0).name          'Resets the Player's Names
-        rdoPlayer2.Text = userInfo(1).name
-        rdoPlayer3.Text = userInfo(2).name
-        rdoPlayer4.Text = userInfo(3).name
-        lblPlayer0Score.Text = userInfo(0).score    'Resets the Player's scores
-        lblPlayer1Score.Text = userInfo(1).score
-        lblPlayer2Score.Text = userInfo(2).score
-        lblPlayer3Score.Text = userInfo(3).score
-        updateUserXML()                 'Updates the User XML File
+        Dim result As Integer = MessageBox.Show("Are you sure?", "Yes", MessageBoxButtons.YesNo) 'Ask the user if they want to delete player
+        If (result = DialogResult.Yes) Then 'Check if they said yes
+            userInfo(playerID).del()        'Deletes the player info selected by selected.
+            rdoPlayer1.Text = userInfo(0).name          'Resets the Player's Names
+            rdoPlayer2.Text = userInfo(1).name
+            rdoPlayer3.Text = userInfo(2).name
+            rdoPlayer4.Text = userInfo(3).name
+            lblPlayer0Score.Text = userInfo(0).score    'Resets the Player's scores
+            lblPlayer1Score.Text = userInfo(1).score
+            lblPlayer2Score.Text = userInfo(2).score
+            lblPlayer3Score.Text = userInfo(3).score
+            updateUserXML()                 'Updates the User XML File
+        End If
     End Sub
 
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
@@ -49,6 +52,5 @@
     Private Sub rdoPlayer1_CheckedChanged(sender As Object, e As EventArgs) Handles rdoPlayer1.CheckedChanged, rdoPlayer2.CheckedChanged, rdoPlayer3.CheckedChanged, rdoPlayer4.CheckedChanged
         playerID = sender.tag
     End Sub
-
 
 End Class
