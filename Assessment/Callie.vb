ef758ae7e00589024545b70dfb5705d40fd2460f
snortings <bjdingley@hotmail.com>
2018-05-24 21:12:32 +1000

    Test

--- Assessment/Callie.vb
+++ Assessment/Callie.vb
@@ -2,6 +2,6 @@
 
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MessageBox.Show("Hello Brandon", "Shawn Mendes", MessageBoxButtons.OK)   'Displays message box to select player slot
+        MessageBox.Show("Hello Brandon", "Shawn Mendes", MessageBoxButtons.OK)   'Displays message box to select player slot'
     End Sub
 End Class
