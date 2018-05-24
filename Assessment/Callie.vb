5bdd5e8b425d3dc46ed1c94a6293b55222ab8fe9
snortings <bjdingley@hotmail.com>
2018-05-24 21:13:35 +1000

    Deleted form

--- Assessment/Callie.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Callie
-
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MessageBox.Show("Hello Brandon", "Shawn Mendes", MessageBoxButtons.OK)   'Displays message box to select player slot'
-    End Sub
-End Class
