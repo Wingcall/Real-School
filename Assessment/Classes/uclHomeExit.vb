bed8fd60b57950d055e7c8356b8baafce17d9206
snortings <bjdingley@hotmail.com>
2018-05-27 19:07:41 +1000

    Push

--- Assessment/Classes/uclHomeExit.vb
+++ Assessment/Classes/uclHomeExit.vb
@@ -3,7 +3,7 @@ Public Class uclHomeExit
 
     Private Sub lblHome_Click(sender As Object, e As EventArgs) Handles lblHome.Click 'Home label clicked
         frmMain.Show() 'Show the main form
-        Dim pForm As Form = sender.Parent.Parent 'Store the current form
+        Dim pForm As Form = sender.Parent.Parent 'Store the current form'
         RemoveHandler pForm.FormClosing, AddressOf formEvents.FormClosing 'Remove the custom close event for the current form
         pForm.Close() 'Close the Current Form
     End Sub
